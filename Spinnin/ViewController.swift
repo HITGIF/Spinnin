ef306885b76157065789d22e0ace84b7b85c5ee8
HITGIF <carbonylgroup@outlook.com>
2017-07-13 20:36:44 +0800

    Initial Commit

---
+++ Spinnin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spinnin
+//
+//  Created by carbonyl on 2017-07-13.
+//  Copyright © 2017 studio.carbonylgroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
